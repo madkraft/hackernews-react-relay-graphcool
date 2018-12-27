da42d91995326b60724b8d22cc7d2ed7868b88b9
anton.kostiuchkov <anton.kostiuchkov@collibra.com>
2018-12-27 16:43:01 +0100

    added typescript

---
+++ src/Environment.js
@@ -0,0 +1,37 @@
+// 1
+const {
+  Environment,
+  Network,
+  RecordSource,
+  Store,
+} = require('relay-runtime')
+
+// 2
+const store = new Store(new RecordSource())
+
+// 3
+const network = Network.create((operation, variables) => {
+  // 4
+  return fetch('__RELAY_API_ENDPOINT__', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      query: operation.text,
+      variables,
+    }),
+  }).then(response => {
+    return response.json()
+  })
+})
+
+// 5
+const environment = new Environment({
+  network,
+  store,
+})
+
+// 6
+export default environment
